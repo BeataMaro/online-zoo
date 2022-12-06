6974c6885f176029b22ac39f597e5099d4b9b1bb
BeataMaro <beatamaro@interia.pl>
2022-12-06 15:18:56 +0100

    feat: implement hamburger menu expanding

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,3 +1,4 @@
+const body = document.body;
 const pagePath = window.location.pathname;
 const navigationLinks = document.querySelectorAll(".nav-item");
 
@@ -7,7 +8,16 @@ navigationLinks.forEach((link) => {
   }
 });
 
-const body = document.body;
+const openMenu = () => body.classList.toggle("open");
+
+const hamburgerMenu = document.getElementById("hamburger-menu");
+
+hamburgerMenu.addEventListener("click", openMenu);
+
+
+
+
+//*******//
 
 const askReviewerForAPatience = () => alert("Online-zoo JS features (week 4) will be done in 5 days. Thank you for patience :)")
 
