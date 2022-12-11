210dc64c14bf2849e41c9af5d9d5e92a4fcafa47
BeataMaro <beatamaro@interia.pl>
2022-12-11 21:15:11 +0100

    fix: add donation amount input error handling

--- pages/donate/script.js
+++ pages/donate/script.js
@@ -5,16 +5,24 @@ const amountForm = document.forms.amountForm;
 amountForm.addEventListener("submit", (e) => e.preventDefault());
 
 const changeDonationValue = (e) => {
-    donation.placeholder = `${e.target.value}`;
-}
+  donation.placeholder = `${e.target.value}`;
+};
 
 const changeRadioOption = (e) => {
-console.log(e.target.value);
-const chosenRadio = Array.from(donationOptions).find(opt => opt.value === e.target.value)
-chosenRadio.checked = true
-}
+  console.log(e.target.value);
+  const chosenRadio = Array.from(donationOptions).find(
+    (opt) => opt.value === e.target.value
+  );
+  if (chosenRadio) {
+    chosenRadio.checked = true;
+  } else {
+    donationOptions.forEach((opt) =>
+      opt.checked === true ? (opt.checked = false) : (opt.checked = false)
+    );
+  }
+};
 
 for (let donationOpt of donationOptions) {
-    donationOpt.addEventListener("click", (e) => changeDonationValue(e));
+  donationOpt.addEventListener("click", (e) => changeDonationValue(e));
 }
 donation.addEventListener("change", (e) => changeRadioOption(e));
